84794636ca80d2b0d7d6be9832fcfcc836cfddbc
Hiroki Shimoie <apelox1gmail.com>
2023-07-11 00:55:46 +0900

    フロントエンドトップページ

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './HomePage.css';
+import ohmine from "../images/ohmine.jpg"
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -9,10 +11,17 @@ const HomePage = () => {
   };
 
   return (
-    <form>
-        <button className="btn btn-primary" onClick={handleButtonClick}>日本酒を検索する</button>
-    </form>
+    <div className='toppage vh-100 d-flex align-items-center justify-content-center'>
+        <div className="card sake-card" style={{width: 18 + "rem"}}>
+            <img src={ohmine} className="card-img-top" alt="大嶺"/>
+            <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 className="card-title">日本酒を探す</h5>
+                <button className="btn btn-primary sake-search-btn" onClick={handleButtonClick}>Go Search</button>              
+            </div>
+        </div>    
+    </div>
   )
+  
 }
 
 export default HomePage
