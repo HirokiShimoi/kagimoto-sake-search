9feec7367891291214cf046218ca6a3ec2b5a09d
HirokiShimoi <you@example.com>
2023-07-11 14:53:06 +0900

    toppage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,14 +11,30 @@ const HomePage = () => {
   };
 
   return (
-    <div className='toppage vh-100 d-flex align-items-center justify-content-center'>
-        <div className="card sake-card" style={{width: 18 + "rem"}}>
-            <img src={ohmine} className="card-img-top" alt="大嶺"/>
-            <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                <h5 className="card-title">日本酒を探す</h5>
-                <button className="btn btn-primary sake-search-btn" onClick={handleButtonClick}>Go Search</button>              
-            </div>
-        </div>    
+    <div>
+      <div className='toppage vh-100 d-flex align-items-center justify-content-center'>
+          <div className="card sake-card m-3" style={{width: 18 + "rem"}}>
+              <img src={ohmine} className="card-img-top" alt="大嶺"/>
+              <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                  <h5 className="card-title">日本酒を探す</h5>
+                  <button className="btn btn-primary sake-search-btn" onClick={handleButtonClick}>Go Search</button>              
+              </div>
+          </div>    
+          <div className="card sake-card m-3" style={{width: 18 + "rem"}}>
+              <img src={ohmine} className="card-img-top" alt="大嶺"/>
+              <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                  <h5 className="card-title">焼酎を探す</h5>
+                  <button className="btn btn-primary sake-search-btn" onClick={handleButtonClick}>Go Search</button>              
+              </div>
+          </div>    
+          <div className="card sake-card m-3" style={{width: 18 + "rem"}}>
+              <img src={ohmine} className="card-img-top" alt="大嶺"/>
+              <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                  <h5 className="card-title">ワインを探す</h5>
+                  <button className="btn btn-primary sake-search-btn" onClick={handleButtonClick}>Go Search</button>              
+              </div>
+          </div>    
+      </div>
     </div>
   )
   
