e3c5b331c236bdf32b2e48e1c09cd40cfda564f4
HirokiShimoi <you@example.com>
2023-08-03 18:21:07 +0900

    ログインフォームのバリデーションとエラーハンドリング

--- src/pages/Logintop.tsx
+++ src/pages/Logintop.tsx
@@ -1,55 +1,62 @@
-import React, { ReactNode, useContext,useEffect } from 'react';
+import React, { ReactNode, useContext, useEffect,useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import { UserContext } from '../contexts/UserContext';
 import './Logintop.css';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import { LinearProgress } from '@mui/material';
 
 
 function Login() {
     const { user,setUser } = useContext(AuthContext);
-    const { setIsLoggedIn,setUsername} = useContext(UserContext);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading,setLoading] = useState(false);
+    const { setIsLoggedIn} = useContext(UserContext);
     const navigate = useNavigate();
-    const {register, handleSubmit, watch, formState: { errors }} = useForm();
-
-    useEffect(() => {
-        console.log(user); // userの値が変更されるたびにログに出力する
-    }, [user]);
+    const {register, handleSubmit, formState: { errors }} = useForm();
 
     const onSubmit = (data: any) => {
+        setLoading(true);
         axios.post('http://localhost:5000/sake/login', data)
         .then(response => {
             if(setUser) {
                 setUser(response.data);
-                console.log(user)
                 setIsLoggedIn(true);
                 localStorage.setItem('token', response.data.token); 
                 navigate("/toppage");                
             }
+            setLoading(false);
         })
         .catch(error => {
-            console.log(error);
+            if(error.response) {
+                setErrorMessage(error.response.data.error);
+            } else {
+                setErrorMessage(error.message);
+            }
+            setLoading(false);
         });
     };
-
     return (
         <div>
-        <div className='form-body'>
-            <div className='form-container'>
-                <h1>酒やの鍵本スタッフ専用</h1>
-                <h1>LOGIN FORM</h1>
-                <form onSubmit={handleSubmit(onSubmit)}> 
-                    <label htmlFor='name'>名前</label>
-                    <input id='name' type='text' {...register("username", { required: "名前は必ず入れてください"})} />
-
-                    <label htmlFor='password'>パスワード</label>
-                    <input id='password' type='password' {...register("password", { required: "パスワードは必ず入れてください"})}  />
-                    <button type='submit'>送信する</button>                    
-                </form>
-                <button type='submit' onClick={() => navigate("/toppage")}>アカウントがない方はこちら</button>
+            <div className='form-body'>
+                <div className='form-container'>
+                    <h1>酒やの鍵本スタッフ専用</h1>
+                    <h1>LOGIN FORM</h1>
+                    <form onSubmit={handleSubmit(onSubmit)}> 
+                        <label htmlFor='name'>名前</label>
+                        <input id='name' type='text' {...register("username", { required: "名前は必ず入れてください"})} />
+                        {errors.username && errors.username.message && <p>{errors.username.message as string}</p>}                    
+                        <label htmlFor='password'>パスワード</label>
+                        <input id='password' type='password' {...register("password", { required: "パスワードは必ず入れてください"})}  />
+                        {errors.password && errors.password.message && <p>{errors.password.message as string}</p>}
+                        <button type='submit'>送信する</button>                    
+                    </form>
+                    {errorMessage && <p>{errorMessage}</p>}
+                    {loading ? <LinearProgress style={{marginTop: '20px', marginBottom: '20px'}} /> : null}
+                    <button type='submit' onClick={() => navigate("/toppage")}>アカウントがない方はこちら</button>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
